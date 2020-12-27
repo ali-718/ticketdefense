e51221108e655d6a2310a53a79b3429e4a922057
Ali Haider <alihaider@Alis-MacBook-Pro.local>
2020-12-28 03:16:23 +0500

    home page done states are added

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
 import { Text, View } from "react-native";
 import Router from "./Router";
+import Home from "./src/Screens/Home";
 
 export default class App extends Component {
   render() {
-    return <Router />;
+    return <Home />;
   }
 }
---
+++ src/Screens/Home.js
@@ -0,0 +1,203 @@
+import React, { Component } from "react";
+import { SafeAreaView, Text, TouchableOpacity, View } from "react-native";
+import { Pink } from "../config/Theme";
+import { Container, Header, Tab, Tabs, TabHeading, Icon } from "native-base";
+import { stateList } from "../config/states";
+
+export default class Home extends Component {
+  componentDidMount() {
+    const search = "f";
+    let find = stateList.filter((item) => item?.text.includes(search));
+
+    console.log(find);
+  }
+
+  tab1 = () => (
+    <View
+      style={{
+        width: "100%",
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <View
+        style={{
+          backgroundColor: "white",
+          borderStyle: "solid",
+          borderWidth: 3,
+          borderColor: "green",
+          borderRadius: 100,
+          alignItems: "center",
+          justifyContent: "center",
+          width: 150,
+          height: 150,
+        }}
+      >
+        <Icon
+          name="thumbs-o-up"
+          type="FontAwesome"
+          style={{ color: Pink, fontSize: 75 }}
+        />
+      </View>
+      <Text style={{ fontWeight: "500", fontSize: 24, marginTop: 20 }}>
+        Keep up the good driving!
+      </Text>
+      <Text
+        style={{
+          fontSize: 15,
+          marginTop: 10,
+          color: "gray",
+          // textAlign: "center",
+          width: "90%",
+        }}
+      >
+        This is where you'll see the status of your active cases, and manage
+        each case from start to finish
+      </Text>
+      <TouchableOpacity
+        style={{
+          width: "90%",
+          height: 50,
+          alignItems: "center",
+          justifyContent: "center",
+          backgroundColor: Pink,
+          borderRadius: 10,
+          marginTop: 20,
+        }}
+      >
+        <Text style={{ color: "white", fontSize: 17 }}>Fight a new ticket</Text>
+      </TouchableOpacity>
+    </View>
+  );
+
+  render() {
+    return (
+      <SafeAreaView
+        style={{
+          width: "100%",
+          flex: 1,
+          alignItems: "center",
+          justifyContent: "center",
+          backgroundColor: "white",
+        }}
+      >
+        <Tabs
+          style={{ borderBottomWidth: 0, backgroundColor: Pink }}
+          tabBarActiveTextColor="white"
+          tabBarPosition="bottom"
+          tabBarUnderlineStyle={{
+            borderBottomWidth: 0,
+            borderBottomColor: "red",
+            backgroundColor: Pink,
+          }}
+        >
+          <Tab
+            tabStyle={{ backgroundColor: Pink }}
+            activeTextStyle={{ color: "white" }}
+            activeTabStyle={{
+              borderBottomWidth: 0,
+              backgroundColor: Pink,
+              borderBottomColor: "red",
+            }}
+            heading={
+              <TabHeading
+                style={{
+                  backgroundColor: Pink,
+                  borderBottomColor: Pink,
+                  flexDirection: "column",
+                }}
+                activeTextStyle={{ color: "white" }}
+                activeTabStyle={{ borderBottomWidth: 0, borderColor: Pink }}
+                textStyle={{ color: "white" }}
+              >
+                <Icon
+                  style={{ color: "white" }}
+                  name="clipboard-text-outline"
+                  type="MaterialCommunityIcons"
+                />
+                <Text style={{ color: "white", marginTop: 2 }}>Cases</Text>
+              </TabHeading>
+            }
+          >
+            {this.tab1()}
+          </Tab>
+          <Tab
+            tabStyle={{ backgroundColor: Pink }}
+            activeTextStyle={{ color: "white" }}
+            activeTabStyle={{
+              borderBottomWidth: 0,
+              backgroundColor: Pink,
+              borderBottomColor: "red",
+            }}
+            heading={
+              <TabHeading
+                style={{
+                  backgroundColor: Pink,
+                  borderBottomColor: Pink,
+                  flexDirection: "column",
+                }}
+                activeTextStyle={{ color: "white" }}
+                activeTabStyle={{ borderBottomWidth: 0, borderColor: Pink }}
+                textStyle={{ color: "white" }}
+              >
+                <View
+                  style={{
+                    width: 60,
+                    height: 60,
+                    backgroundColor: Pink,
+                    borderStyle: "solid",
+                    borderWidth: 5,
+                    borderColor: "white",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    borderRadius: 100,
+                    marginTop: -50,
+                  }}
+                >
+                  <Icon
+                    style={{ color: "white", fontSize: 20 }}
+                    name="plus"
+                    type="AntDesign"
+                  />
+                </View>
+              </TabHeading>
+            }
+          >
+            {this.tab1()}
+          </Tab>
+          <Tab
+            tabStyle={{ backgroundColor: Pink }}
+            activeTextStyle={{ color: "white" }}
+            activeTabStyle={{
+              borderBottomWidth: 0,
+              backgroundColor: Pink,
+              borderBottomColor: "red",
+            }}
+            heading={
+              <TabHeading
+                style={{
+                  backgroundColor: Pink,
+                  borderBottomColor: Pink,
+                  flexDirection: "column",
+                }}
+                activeTextStyle={{ color: "white" }}
+                activeTabStyle={{ borderBottomWidth: 0, borderColor: Pink }}
+                textStyle={{ color: "white" }}
+              >
+                <Icon
+                  style={{ color: "white" }}
+                  name="clipboard-text-outline"
+                  type="MaterialCommunityIcons"
+                />
+                <Text style={{ color: "white", marginTop: 2 }}>Cases</Text>
+              </TabHeading>
+            }
+          >
+            {this.tab1()}
+          </Tab>
+        </Tabs>
+      </SafeAreaView>
+    );
+  }
+}
