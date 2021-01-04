4b9e478ec56142d1db0880c7c325f1e48db3e3b6
Ali Haider <alihaider@Alis-MacBook-Pro.local>
2021-01-05 00:14:59 +0500

    issues fixed

--- src/Screens/Auth/Splash.js
+++ src/Screens/Auth/Splash.js
@@ -4,28 +4,41 @@ import * as SplashScreen from "expo-splash-screen";
 import { connect } from "react-redux";
 import { mapStateToProps } from "../../config/config";
 import { fetchUser } from "../../redux/actions/AuthActions";
+import * as Animatable from "react-native-animatable";
+import { Pink } from "../../config/Theme";
 
 class Splash extends Component {
   componentDidMount() {
-    SplashScreen.preventAutoHideAsync();
-    this.props
-      .fetchUser()
-      .then(() => {
-        SplashScreen.hideAsync();
-      })
-      .catch(() => {
-        SplashScreen.hideAsync();
-
-        this.props.navigation.navigate("onBoard");
-      });
+    // SplashScreen.preventAutoHideAsync();
+    setTimeout(() => {
+      this.props
+        .fetchUser()
+        .then(() => {
+          SplashScreen.hideAsync();
+        })
+        .catch(() => {
+          SplashScreen.hideAsync();
+          this.props.navigation.navigate("onBoard");
+        });
+    }, 1000);
   }
 
   render() {
     return (
-      <View style={{ width: "100%", flex: 1 }}>
-        <Image
-          style={{ width: "100%", flex: 1, resizeMode: "cover" }}
-          source={require("../../../assets/splash.png")}
+      <View
+        style={{
+          width: "100%",
+          flex: 1,
+          backgroundColor: Pink,
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Animatable.Image
+          duration={1000}
+          animation="bounceIn"
+          style={{ width: 450, height: 150, resizeMode: "contain" }}
+          source={require("../../../assets/logo.png")}
         />
       </View>
     );
