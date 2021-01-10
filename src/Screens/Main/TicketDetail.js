0a7b2e708ab8d2dd337a8a5e093213352508767f
Ali Haider <alihaider@Alis-MacBook-Pro.local>
2021-01-11 02:07:21 +0500

    forgot done, working on ticket detail

--- src/Screens/Main/TicketDetail.js
+++ src/Screens/Main/TicketDetail.js
@@ -55,11 +55,85 @@ export default class TicketDetail extends Component {
             }}
           >
             <View style={{ width: "90%" }}>
-              <Text
-                style={{ fontSize: 22, color: "black", fontWeight: "bold" }}
-              >
+              <Text style={{ fontSize: 22, color: "black" }}>
                 Your ticket from{" "}
+                {moment(this.state.ticket?.date).format("MMMM D")}
               </Text>
+
+              <View
+                style={{
+                  width: "100%",
+                  backgroundColor: "rgb(242, 201, 223)",
+                  padding: 15,
+                  marginTop: 20,
+                  alignItems: "center",
+                }}
+              >
+                <View
+                  style={{
+                    width: "100%",
+                    flexDirection: "row",
+                    alignItems: "center",
+                  }}
+                >
+                  <View
+                    style={{
+                      width: 70,
+                      alignItems: "flex-start",
+                      justifyContent: "center",
+                    }}
+                  >
+                    <Image
+                      style={{ width: 60, height: 60, borderRadius: 100 }}
+                      source={{ uri: this.state.ticket?.lawyer?.image }}
+                    />
+                  </View>
+                  <View style={{ flex: 1, marginLeft: 5 }}>
+                    <Text
+                      numberOfLines={1}
+                      style={{
+                        color: "black",
+                        fontSize: 13,
+                        fontWeight: "bold",
+                        width: "90%",
+                      }}
+                    >
+                      Handling your case
+                    </Text>
+                    <Text
+                      numberOfLines={1}
+                      style={{
+                        color: "black",
+                        fontSize: 14,
+                        width: "100%",
+                        marginTop: 5,
+                      }}
+                    >
+                      {this.state.ticket?.lawyer?.name}
+                    </Text>
+                  </View>
+                </View>
+                <TouchableOpacity
+                  // onPress={() => this.setState({ existingCardModal: true })}
+                  style={{
+                    width: "100%",
+                    height: 50,
+                    alignItems: "center",
+                    justifyContent: "center",
+                    borderRadius: 10,
+                    borderColor: Pink,
+                    borderWidth: 1,
+                    borderStyle: "solid",
+                    marginTop: 20,
+                  }}
+                >
+                  <Text
+                    style={{ color: Pink, fontSize: 17, fontWeight: "bold" }}
+                  >
+                    Message Attorney
+                  </Text>
+                </TouchableOpacity>
+              </View>
             </View>
           </View>
         </ScrollView>
